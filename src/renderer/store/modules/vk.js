7953608c98f064b8778c73b877462fc42239e530
artur.abdurakhmanov <artur.abdurakhmanov@nexign-systems.com>
2019-01-22 23:45:50 +0300

    add jsonp

--- src/renderer/store/modules/vk.js
+++ src/renderer/store/modules/vk.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { SET_SEARCH } from '../const';
+import jsonp from 'axios-jsonp';
+// import { SET_SEARCH } from '../const';
 
 const state = {
   searchResult: {},
@@ -7,15 +8,19 @@ const state = {
 
 const actions = {
   getSearch({ commit }, { slug }) {
-    axios.get(`https://api.vk.com/method/newsfeed.search?q="${slug}"&access_token=a74abc42dce52ec3e599c0a713c4cdf670a492f380e3fe04431334dc3eb254c1c4592fbddaaa0ffbbd542&v=5.92`)
-      .then((response) => {
-        commit('SET_SEARCH', { result: response.data });
-      });
+    const url = `https://api.vk.com/method/newsfeed.search?q='%23${slug}'&access_token=a74abc42dce52ec3e599c0a713c4cdf670a492f380e3fe04431334dc3eb254c1c4592fbddaaa0ffbbd542&v=5.92`;
+    console.log(slug, url);
+    axios({
+      url,
+      adapter: jsonp,
+    }).then((response) => {
+      commit('setSearch', { result: response.data.response.items });
+    });
   },
 };
 
 const mutations = {
-  [SET_SEARCH](state, { result }) {
+  setSearch(state, { result }) {
     console.log(result);
     state.searchResult = result;
   },
